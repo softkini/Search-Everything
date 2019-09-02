e514e1d017c19340f9a5c24d28c5909f53e2d0a9
Sabbir Hossain Pranto <otnarp007@gmail.com>
2019-09-02 14:18:16 +0600

    First Commit

---
+++ Main.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Search_Everything
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbDrive = new System.Windows.Forms.ComboBox();
+            this.lstFiles = new System.Windows.Forms.ListView();
+            this.colNume = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cbDrive
+            // 
+            this.cbDrive.FormattingEnabled = true;
+            this.cbDrive.Location = new System.Drawing.Point(12, 10);
+            this.cbDrive.Name = "cbDrive";
+            this.cbDrive.Size = new System.Drawing.Size(150, 21);
+            this.cbDrive.TabIndex = 7;
+            this.cbDrive.SelectedIndexChanged += new System.EventHandler(this.cbDrive_SelectedIndexChanged);
+            // 
+            // lstFiles
+            // 
+            this.lstFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colNume,
+            this.colPath});
+            this.lstFiles.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstFiles.FullRowSelect = true;
+            this.lstFiles.GridLines = true;
+            this.lstFiles.HideSelection = false;
+            this.lstFiles.Location = new System.Drawing.Point(12, 39);
+            this.lstFiles.MultiSelect = false;
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(768, 231);
+            this.lstFiles.TabIndex = 6;
+            this.lstFiles.UseCompatibleStateImageBehavior = false;
+            this.lstFiles.View = System.Windows.Forms.View.Details;
+            // 
+            // colNume
+            // 
+            this.colNume.Text = "Name";
+            this.colNume.Width = 200;
+            // 
+            // colPath
+            // 
+            this.colPath.Text = "Path";
+            this.colPath.Width = 700;
+            // 
+            // bSearch
+            // 
+            this.bSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(223)))), ((int)(((byte)(125)))));
+            this.bSearch.FlatAppearance.BorderSize = 0;
+            this.bSearch.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(223)))), ((int)(((byte)(139)))));
+            this.bSearch.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(213)))), ((int)(((byte)(134)))));
+            this.bSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bSearch.Location = new System.Drawing.Point(424, 8);
+            this.bSearch.Name = "bSearch";
+            this.bSearch.Size = new System.Drawing.Size(75, 23);
+            this.bSearch.TabIndex = 5;
+            this.bSearch.Text = "Search";
+            this.bSearch.UseVisualStyleBackColor = false;
+            this.bSearch.Click += new System.EventHandler(this.bSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(168, 10);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 20);
+            this.txtSearch.TabIndex = 4;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 282);
+            this.Controls.Add(this.cbDrive);
+            this.Controls.Add(this.lstFiles);
+            this.Controls.Add(this.bSearch);
+            this.Controls.Add(this.txtSearch);
+            this.MinimumSize = new System.Drawing.Size(808, 321);
+            this.Name = "Main";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Search Everything - Softkini.com";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbDrive;
+        private System.Windows.Forms.ListView lstFiles;
+        private System.Windows.Forms.ColumnHeader colNume;
+        private System.Windows.Forms.ColumnHeader colPath;
+        private System.Windows.Forms.Button bSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Search_Everything
+{
+    public partial class Main : Form
+    {
+        public string FileSearch = "";
+        public static ListViewItem item;
+
+        private DriveInfo[] Drives = DriveInfo.GetDrives();
+        private string[] strDrive = new string[5];
+        private int count = 0;
+        public int curent = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+            foreach (DriveInfo d in Drives)
+            {
+                if (d.IsReady)
+                {
+                    strDrive[count] = d.Name; count++;
+                    cbDrive.Items.Add(d.VolumeLabel + "( " + d.Name + " )");
+                }
+            }
+            cbDrive.Text = (string)cbDrive.Items[0];
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GetAllFiles(DirectoryInfo di)
+        {
+            try
+            {
+                Application.DoEvents();
+                foreach (FileInfo file in di.GetFiles(FileSearch))
+                {
+                    if ((file.Attributes & FileAttributes.Hidden) == 0)
+                    {
+                        item = lstFiles.Items.Add(file.Name);
+                        item.SubItems.Add(file.FullName);
+                    }
+                }
+                foreach (DirectoryInfo director in di.GetDirectories())
+                {
+                    if ((director.Attributes & FileAttributes.Hidden) == 0) GetAllFiles(director);
+                }
+
+            }
+            catch 
+            {
+                //
+            }
+        }
+
+        public void Execute(string cmd)
+        {
+            if (cmd != string.Empty)
+            {
+                Process proc = new Process();
+                proc.EnableRaisingEvents = false;
+                proc.StartInfo.FileName = cmd;
+                proc.Start(); proc.Close();
+                proc.Dispose();
+            }
+        }
+
+        private void bSearch_Click(object sender, EventArgs e)
+        {
+            lstFiles.Items.Clear();
+            this.Text = "Searching ...";
+            FileSearch = txtSearch.Text + "*";
+            GetAllFiles(new DirectoryInfo(strDrive[curent]));
+            this.Text = string.Format("Found = {0} items", lstFiles.Items.Count);
+        }
+
+        private void cbDrive_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            curent = cbDrive.SelectedIndex;
+        }
+
+        private void lstFiles_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Execute(lstFiles.SelectedItems[0].SubItems[1].Text);
+        }
+    }
+}
